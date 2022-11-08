eb03cdf138309d0c0be59ed77a77b25b3b6a93cf
urbluemn <nyaroshko1404@gmail.com>
2022-11-08 04:05:51 +0300

    understand 50/50 need explanation on where to take anth

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -1,4 +1,7 @@
-﻿public enum Operation
+﻿using System.Security.Cryptography.X509Certificates;
+
+
+public enum Operation
 {
     Sum,
     Subtract,
@@ -8,36 +11,28 @@ public class OperationManager
 {
     private int _first;
     private int _second;
+    private ExecutionManager _executionManager;
     public OperationManager(int first, int second)
     {
         _first = first;
         _second = second;
+        _executionManager = new ExecutionManager(this);
     }
-    private int Sum()
+    public int Sum()
     {
         return _first + _second;
     }
-    private int Subtract()
+    public int Subtract()
     {
         return _first - _second;
     }
-    private int Multiply()
+    public int Multiply()
     {
         return _first * _second;
     }
     public int Execute(Operation operation)
     {
-        switch (operation)
-        {
-            case Operation.Sum:
-                return Sum();
-            case Operation.Subtract:
-                return Subtract();
-            case Operation.Multiply:
-                return Multiply();
-            default:
-                return -1; //just to simulate
-        }
+        return _executionManager.FuncExecute[operation].Invoke();
     }
 }
 
@@ -46,17 +41,24 @@ public class ExecutionManager
 {
     public Dictionary<Operation, Func<int>> FuncExecute { get; set; }
     //Add delegates for sum, substrat and multiply here
-    public ExecutionManager()
-    {
+    private OperationManager _operationManager;
 
+    public ExecutionManager(OperationManager operationManager)
+    {
+        _operationManager = operationManager;
+        FuncExecute = new Dictionary<Operation, Func<int>>();
+        PrepareExecution();
     }
 
-    public void PopulateFunctions(//pass delegates and register)
+    public void PopulateFunctions(Operation op, Func<int> func)
     {
-
+        FuncExecute.Add(op, func);
     }
     public void PrepareExecution()
     {
+        FuncExecute.Add(Operation.Sum, _operationManager.Sum);
+        FuncExecute.Add(Operation.Subtract, _operationManager.Subtract);
+        FuncExecute.Add(Operation.Multiply, _operationManager.Multiply);
         //fill dictionary here
     }
 }
@@ -66,8 +68,10 @@ class Program
     static void Main(string[] args)
     {
         var opManager = new OperationManager(20, 10);
-        var result = opManager.Execute(Operation.Sum);
-        Console.WriteLine($"The result of the operation is {result}");
+        
+        Console.WriteLine($"The result of the operation Sum is {opManager.Execute(Operation.Sum)}");
+        Console.WriteLine($"The result of the operation Substract is {opManager.Execute(Operation.Subtract)}");
+        Console.WriteLine($"The result of the operation Multiply is {opManager.Execute(Operation.Multiply)}");
         Console.ReadKey();
     }
 }
